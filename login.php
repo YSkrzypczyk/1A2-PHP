5320703a3b0632f104001942ff2fd78c95002733
skrzypczyk <ysk@wizards-technologies.com>
2023-03-28 19:45:34 +0200

    tp login

--- login.php
+++ login.php
@@ -7,5 +7,22 @@
 		</div>
 	</div>
 
+
+<?php
+/*
+Consigne du TP
+	- Créer un formulaire HTML permettant de se connecter
+	- Les données du formulaire devront être envoyées au fichier login.php
+	- Vérification des données saisies
+	- Affichages des erreurs dans une div alert (Bootstrap)
+	- Dans le cas ou les données sont OK, créer une variable de SESSION contenant :
+	----> email = y.skrz......
+	----> login = true
+	- redirection sur la page index.php
+*/
+?>
+
+
+
 	
 <?php include "template/footer.php";?>
